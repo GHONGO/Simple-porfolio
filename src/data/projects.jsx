c0e8127cf2f6280843812f422c6901c8c5c50633
GHONGO <griffinwhongo@gmail.com>
2025-12-24 12:35:59 +0300

    Update projects.jsx
    Changed the images and links

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,7 +1,7 @@
-import jobhunt from '../assets/jobhunt.webp';
-import portfolio from '../assets/portfolio.webp';
-import Cinemaplus from '../assets/Cinemaplus.webp';
-import heavenly from '../assets/heavenly.webp';
+import WasteMap from '../assets/Wastemap.png';
+import NGO from '../assets/NGO.png';
+import Chatapp from '../assets/Chatapp.png';
+import Gymplanner from '../assets/Gymplanner.png';
 
 const projects = [
     {
@@ -10,7 +10,7 @@ const projects = [
         summary: 'WasteMap is a full-stack web application that empowers communities and municipalities to manage waste efficiently. Citizens can report waste issues with photos and GPS locations, while municipal authorities can assign cleanup teams, optimize routes, and monitor cleanup performance in real time.',
         tech: ['React', 'Express', 'MongoDB'],
         link: 'https://wastemap.netlify.app/',
-        image: Wastemap
+        image: WasteMap  
     },
     {
         id: 2,
@@ -18,7 +18,7 @@ const projects = [
         summary: 'This is a non-profit organization aimed at Providing African Solutions to African Challenges (PASTAC), through Leadership, Education & Legislation. And Promoting STARNORMICS and the Making of STARNORMS and, the promotion of fairness and justice.',
         tech: ['Wordpress'],
         link: 'https://drkennedyhongofoundation.org/',
-        image: Dr.kennedyfoundation
+        image: NGO  
     },
     {
         id: 3,
@@ -31,8 +31,7 @@ const projects = [
     {
         id: 4,
         title: 'GOLDEN FITNESS',
-        // login: 'username:demo password:demo',
-        summary: ' A modern full-stack fitness platform built with the MERN stack. Designed to help users create personalized workout routines, plan budget-friendly diets, and interact within a growing fitness community.',
+        summary: 'A modern full-stack fitness platform built with the MERN stack. Designed to help users create personalized workout routines, plan budget-friendly diets, and interact within a growing fitness community.',
         tech: ['React', 'Tailwind/CSS', 'Express.js', 'MongoDb','Node.js', 'Socket.io'],
         link: '#',
         image: Gymplanner
